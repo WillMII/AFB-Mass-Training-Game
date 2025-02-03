77f422f4228da71b085c3c35f2c9389e5c3402e8
emilyasperring <sperring4@gmail.com>
2025-02-03 13:35:02 -0500

    Set up Routes & Folders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="mb-4"> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<ErrorPage />} /> 
+      </Routes>
+    </Container>
   );
 }
 
---
+++ frontend/src/pages/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1> Invalid Link :( </h1>
+            <h2> Please double check your spelling or link </h2>
+        </div>
+    );
+}
+
+export default ErrorPage;
