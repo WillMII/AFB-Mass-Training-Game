14812624afb8cfb19f78c242be304813bc48563a
emilyasperring <sperring4@gmail.com>
2025-02-05 18:01:49 -0500

    feat: added footer basics

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap';
 import Hdr from '../components/Hdr'
 import ProgressCenter from '../components/ProgressCenter';
 import ModuleCard from '../components/ModuleCard';
+import Footer from '../components/Footer';
 
 const Home = () => {
 
@@ -14,21 +15,24 @@ const Home = () => {
 
     return (
         <>
-            <Hdr />
-            <Container >
-                <div className='my-5'>
-                    <h1 className='text-primary text-decoration-underline'>Progress Center</h1>
-                    <ProgressCenter />
-                </div>
-                <div>
-                    <h1 className='text-primary text-decoration-underline'>Training Modules</h1>
-                    <div class="d-flex justify-content-between align-self-stretch wrap">
-                        {modules.map((module, index) => (
-                            <ModuleCard key={index} title={module.title} desc={module.desc} />
-                        ))}
+            <div className="d-flex flex-column min-vh-100">
+                <Hdr />
+                <Container className="flex-grow-1">
+                    <div className='my-5'>
+                        <h1 className='text-primary text-decoration-underline'>Progress Center</h1>
+                        <ProgressCenter />
                     </div>
-                </div>
-            </Container>
+                    <div>
+                        <h1 className='text-primary text-decoration-underline'>Training Modules</h1>
+                        <div class="d-flex justify-content-between align-self-stretch wrap">
+                            {modules.map((module, index) => (
+                                <ModuleCard key={index} title={module.title} desc={module.desc} />
+                            ))}
+                        </div>
+                    </div>
+                </Container>
+                <Footer />
+            </div>
         </>
     )
 }
