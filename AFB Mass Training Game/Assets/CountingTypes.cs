8ecaceab82c713659d5f8df4d2db416bac28c881
WillMII <wam79269@uga.edu>
2025-02-28 12:12:09 -0500

    more module 1

--- AFB Mass Training Game/Assets/CountingTypes.cs
+++ AFB Mass Training Game/Assets/CountingTypes.cs
@@ -7,15 +7,30 @@ using TMPro;
 
 public class CountingTypes : MonoBehaviour
 {
-    public CluePart script1;
-    private bool found1;
+    public CluePart TO;
+    public CluePart PO;
+    public CluePart ED;
+    public CluePart SW;
+    public CluePart TD;
+    public CluePart TDIP;
+    public CluePart Brief;
+
+    private bool[] found = new bool[7];
     public TMP_Text text;
     private int numFound;
+    private int i;
 
     // Start is called before the first frame update
     void Start()
     {
-        found1 = script1.getAlrClk();
+        found[0] = TO.getAlrClk();
+        found[1] = PO.getAlrClk();
+        found[2] = ED.getAlrClk();
+        found[3] = SW.getAlrClk();
+        found[4] = TD.getAlrClk();
+        found[5] = TDIP.getAlrClk();
+        found[6] = Brief.getAlrClk();
+
         //text = this.gameObject.GetComponent<TMP_Text>();
         numFound = 0;
     }
@@ -23,11 +38,17 @@ public class CountingTypes : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        found1 = script1.getAlrClk();
-        if (found1 == true)
+        numFound = 0;
+        i = 0;
+        while (i < 7)
         {
-            numFound = 1;
+            if (found[i])
+            {
+                numFound += 1;
+            }
+            i++;
         }
+        
         text.text = "STINFO Types Found: " + numFound +"/7";
     }
 }
