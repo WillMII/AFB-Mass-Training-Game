3e5c0ffdd16a1c20ce7d7bcd8018763def5c54dc
WillMII <wam79269@uga.edu>
2025-04-08 23:42:55 -0400

    module 2 progress

--- AFB Mass Training Game/Assets/ExitRoom2.cs
+++ AFB Mass Training Game/Assets/ExitRoom2.cs
@@ -13,6 +13,7 @@ public class ExitRoom2 : MonoBehaviour
     public CountingTypes2 counter;
 
     public GameObject finishText;
+    public Keypad Keypad;
 
     private bool canExit;
     // Start is called before the first frame update
@@ -32,7 +33,8 @@ public class ExitRoom2 : MonoBehaviour
 
     void OnMouseDown()
     {
-        canExit = Keyboard.getAlrClk() && safe.getAlrInst() && ideaPoster.getAlrInst() && trashCan.getAlrInst() && counter.getNumFound() == 4;
+        canExit = Keypad.getCanExit();
+        //canExit = Keyboard.getAlrClk() && safe.getAlrInst() && ideaPoster.getAlrInst() && trashCan.getAlrInst() && counter.getNumFound() == 4;
         if (canExit)
         {
             Instantiate(finishText.gameObject);
